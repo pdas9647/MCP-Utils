0c2484bb709864ea55419481432c7595440e6916
arka-bhat <arka.bhattacharya33@gmail.com>
2025-06-21 01:06:52 +0530

    better import routing

---
+++ utils/index.ts
@@ -0,0 +1,7 @@
+export * from "./db";
+export * from "./encryption";
+export * from "./killPortOnLaunch";
+export * from "./printInConsole";
+export * from "./sendError";
+export * from "./updateClaudeConfig";
+// Add more exports as needed for each utility file
